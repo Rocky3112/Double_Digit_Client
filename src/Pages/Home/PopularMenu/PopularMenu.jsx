16507d6fcc77fa808a13461be561e9dd29b91f32
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-07-12 14:24:02 +0600

    popular item fetched

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuItems from "../../Shared/MenuItems/MenuItems";
 
 
 const PopularMenu = () => {
+const [ menu, setMenu] = useState([])
+    useEffect( ()=>{
+        fetch('menu.json')
+        .then(res=>res.json())
+        .then(data =>{
+            const popularItems = data.filter(item =>item.category ==='popular')
+            setMenu(popularItems)
+        })
+    }, [])
     return (
         <section className="mt-48">
         <SectionTitle
         heading={"Popular Items"}
         subHeading={"From Our Menu"}
         ></SectionTitle>
+
+        <div>
+            {
+                menu.map(item =><MenuItems
+                key ={item._id}
+                item ={item}
+                ></MenuItems>)
+            }
+        </div>
         </section>
+        
     );
 };
 
---
+++ src/Pages/Shared/MenuItems/MenuItems.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
+
+const MenuItems = ({item}) => {
+    const {name, image,price,recipe } = item;
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MenuItems;
