73409c71ad0cc124146d3fc236a4022c630ddba5
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-07-16 21:16:45 +0600

    table added inside myCart

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -1,16 +1,66 @@
+/* eslint-disable no-unused-vars */
 import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
+import { FaTrashAlt } from 'react-icons/fa';
 
 const MyCart = () => {
-    const [cart] =useCart();
-    const total = cart.reduce((sum, item)=>item.price + sum, 0)
-    return (
-        <div>
+  const [cart] = useCart();
+  const total = cart.reduce((sum, item) => item.price + sum, 0);
+
+  const handleDelete=(item)=>{
+
+  }
+  return (
+    <div className="w-full">
             <Helmet>
-                <title>Bistro Boss | MyCart</title>
+                <title>Bistro Boss | My Cart</title>
             </Helmet>
-            <h2 className=" text-3xl">Total Cart = {cart.length}</h2>
-            <h2 className=" text-3xl">Total Price = $ {total}</h2>
+            <div className="uppercase font-semibold h-[60px] flex justify-evenly items-center">
+                <h3 className="text-3xl">Total Items: {cart.length}</h3>
+                <h3 className="text-3xl">Total Price: ${total}</h3>
+                <button className="btn btn-warning btn-sm">PAY</button>
+            </div>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Food</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => <tr
+                                key={item._id}
+                            >
+                                <td>
+                                    {index + 1}
+                                </td>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {item.name}
+                                </td>
+                                <td className="text-end">${item.price}</td>
+                                <td>
+                                    <button onClick={() => handleDelete(item)} className="btn btn-ghost bg-red-600  text-white"><FaTrashAlt></FaTrashAlt></button>
+                                </td>
+                            </tr>)
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
