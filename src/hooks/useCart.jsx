4af9024d0f513b740ff27bf103511ce260c70017
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-07-22 22:03:29 +0600

    usecart

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,19 +1,28 @@
 import { useQuery } from '@tanstack/react-query'
-import { useContext } from 'react';
-import { AuthContext } from '../Providers/AuthProvider';
-const useCart = () =>{
-
-    const {user}=useContext(AuthContext);
-
+import useAxiosSecure from './useAxiosSecure';
+import useAuth from './useAuth';
+const useCart = () => {
+    const { user, loading } = useAuth();
+    // const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxiosSecure();
     const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
-        queryFn: async ()=> {
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
-            return res.json();
-        }
-      })
-      return [ cart,refetch]
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axiosSecure(`/carts?email=${user?.email}`)
+            console.log('res from axios', res)
+            return res.data;
+        },
+    })
+
+    return [cart, refetch]
 
 }
 export default useCart;
 
+// queryFn: async () => {
+        //     const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, { headers: {
+        //         authorization: `bearer ${token}`
+        //     }})
+        //     return res.json();
+        // },
