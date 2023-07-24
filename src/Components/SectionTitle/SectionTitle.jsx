a32126b62fe148bec2ba090ab0a573133fc4c2e6
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-07-24 21:47:55 +0600

    deleted successful from manageItems

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -4,8 +4,8 @@
 const SectionTitle = ({heading, subHeading}) => {
     return (
         <div className="mx-auto text-center md:w-4/12 my-4">
-            <h2 className="text-yellow-500 py-2"> --- {subHeading} --- </h2>
-            <p className="text-4xl uppercase font-semibold border-y-2 ">{heading}</p>
+            <h2 className="text-yellow-500 py-2 text-xl"> --- {subHeading} --- </h2>
+            <p className="text-3xl uppercase font-semibold border-y-2 py-2 ">{heading}</p>
         </div>
     );
 };
