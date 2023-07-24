9e2677a8234661c8b1ce212236ecc84d9604e626
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-07-25 00:59:31 +0600

    create a stripe payment

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,11 +1,22 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
 
 const Payment = () => {
-    return (
-        <div className="w-full">
-            <SectionTitle subHeading={'Please Provide'} heading={'Payment'}></SectionTitle>
-        </div>
-    );
+  const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
+  return (
+    <div className="w-full">
+      <SectionTitle
+        subHeading={"Please Provide"}
+        heading={"Payment"}
+      ></SectionTitle>
+
+      <Elements stripe={stripePromise}>
+        <CheckoutForm></CheckoutForm>
+      </Elements>
+    </div>
+  );
 };
 
 export default Payment;
