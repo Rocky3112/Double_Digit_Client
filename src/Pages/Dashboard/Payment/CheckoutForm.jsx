2e8cffe3572c661f09542083d1260837b050f107
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-07-25 19:02:35 +0600

    works for payment & almost done

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -54,7 +54,50 @@ const CheckoutForm = ({ cart, price }) => {
 
       setProcessing(true)
 
-    
+      const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+          clientSecret,
+          {
+              payment_method: {
+                  card: card,
+                  billing_details: {
+                      email: user?.email || 'unknown',
+                      name: user?.displayName || 'anonymous'
+                  },
+              },
+          },
+      );
+
+      if (confirmError) {
+          console.log(confirmError);
+      }
+
+      console.log('payment intent', paymentIntent)
+      setProcessing(false)
+      if (paymentIntent.status === 'succeeded') {
+          setTransactionId(paymentIntent.id);
+          // save payment information to the server
+          const payment = {
+              email: user?.email,
+              transactionId: paymentIntent.id,
+              price,
+              date: new Date(),
+              quantity: cart.length,
+              cartItems: cart.map(item => item._id),
+              menuItems: cart.map(item => item.menuItemId),
+              status: 'service pending',
+              itemNames: cart.map(item => item.name)
+          }
+          axiosSecure.post('/payments', payment)
+              .then(res => {
+                  console.log(res.data);
+                  if (res.data.result.insertedId) {
+                      // display confirm
+                  }
+              })
+      }
+
+
+  }
 
   return (
       <>
