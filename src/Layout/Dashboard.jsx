9f75095d5b1ce399644d0a18a184e5a5db84440e
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-07-26 13:22:48 +0600

    displayed chart inside admin home page

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -35,7 +35,7 @@ const Dashboard = () => {
             {isAdmin ? (
               <>
                 <li>
-                  <NavLink to="/dashboard/home">
+                  <NavLink to="/dashboard/adminhome">
                     <FaHome></FaHome> Admin Home
                   </NavLink>
                 </li>
@@ -64,7 +64,7 @@ const Dashboard = () => {
             ) : (
               <>
                 <li>
-                  <NavLink to="/dashboard/home">
+                  <NavLink to="/dashboard/userhome">
                     <FaHome></FaHome> User Home
                   </NavLink>
                 </li>
